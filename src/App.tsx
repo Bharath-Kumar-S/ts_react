5e8773d01618a094c9b2e23b3bcd687cda7a23cf
Bharath-Kumar-S <bharathkumarkarthick@gmail.com>
2022-07-27 23:56:50 +0300

    ts setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Greet from './components/Greet';
+
+function App() {
+  return (
+    <div className="App">
+      <Greet name='Ramya'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Greet.tsx
@@ -0,0 +1,11 @@
+type GreetProps = {
+    name: string
+}
+
+const Greet = ({name}: GreetProps) => {
+  return (
+    <div>Greet {name}</div>
+  )
+}
+
+export default Greet
